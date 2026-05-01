48ab0a819201e88571399b22279ec1dd4efcf835
ashrafultusar <ashrafulislamtusar@gmail.com>
2026-05-02 01:44:07 +0600

    addeed all

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig = {
     },
   },
   images: {
+    loader: 'cloudinary', // এটি যোগ করুন
+    path: 'https://res.cloudinary.com/dzv04zugz/image/upload/', // এটি যোগ করুন
     remotePatterns: [
       {
         protocol: 'https',
